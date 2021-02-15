fc229eb9f09b2ddb33db253041d036c5791b2c01
Buddy Scott <jamesscott@college.harvard.edu>
2021-02-15 18:07:22 -0500

    more data!

--- prelim.R
+++ prelim.R
@@ -7,7 +7,16 @@ library(readr)
 # through 2/15/21, the other is NBA games in the 2019-2020 season through a
 # similar time frame (roughly 27 games).
 
-ctg_20_21 <- read.csv("league_summary_20_21.csv")
-ctg_19_20 <- read.csv("league_summary_19_20.csv")
+summary_20_21 <- read.csv("league_summary_20_21.csv")
+summary_19_20 <- read.csv("league_summary_19_20.csv")
+
+four_factors_20_21 <- read.csv("four_factors_20_21.csv")
+four_factors_19_20 <- read.csv("four_factors_19_20.csv")
+
+shooting_20_21 <- read.csv("shooting_20_21.csv")
+shooting_19_20 <- read.csv("shooting_19_20.csv")
+
+play_context_20_21 <- read.csv("play_context_20_21.csv")
+play_context_19_20 <- read.csv("play_context_19_20.csv")
 
 
